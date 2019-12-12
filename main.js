8955d121db14e697b5f59cfc78223ed1895e49e7
francesco <fra>
2019-12-12 15:36:03 +0100

    working \n in chat

--- main.js
+++ main.js
@@ -12,30 +12,24 @@ bot.use(session());
 
 
 
-function cut(toRemove,string){
-  var ret = string.replace(toRemove,'');
+function cut(toRemove, string) {
+  var ret = string.replace(toRemove, '');
   return ret;
 }
 
-function getUserMsg(context){
+function getUserMsg(context) {
 
-  return JSON.stringify(context.message.text);
+  return context.message.text;
 
 }
 
-/*bot.start('', (ctx) => {
-  ctx.session.counter = ctx.session.counter || 0
-  ctx.session.counter++
-
-});*/
-
-function from(context){
+function from(context) {
 
   return context.from.id;
 
 }
 
-function getChatID(context){
+function getChatID(context) {
   return context.chat.id;
 }
 
@@ -46,16 +40,25 @@ bot.hears('Sei tu un bug ', (ctx) => {
 
 });
 
+
+
+
+
+
 bot.hears('buildnote', (ctx) => {
   ctx.reply(A);
-
-  //ctx.reply('DEVO MORIRE! '+ getUserMsgToBotId(ctx) );
-
 });
+
+bot.hears('brb', (ctx) => {
+  ctx.reply("torno subito");
+});
+
+
+
 bot.hears('@nunocodex', (ctx) => {
 
 
-  ctx.reply('prima o poi risponderà' );
+  ctx.reply('prima o poi risponderà \no forse no');
 
 });
 
@@ -63,39 +66,39 @@ bot.hears('@nunocodex', (ctx) => {
 bot.start((ctx) => {
   ctx.session.wasadmin = false;
   bot.telegram.getChatAdministrators(getChatID(ctx))
-  .then( function(data) {
-var wasadmin = false;
-    var n = Object.keys(data).length;
-  for (var i = 0; i < n && i < 10; i++){
-      var admin = parseInt(data[i].user.id);
-      var user = parseInt(ctx.from.id);
-    if(admin ==  user){
-      /*ctx.reply('Benvenuto Amministratore ');*/
-      wasadmin = true;
-      break;
-    }else{
-      /*ctx.reply('non sei un admin, devi morire!');*/
-      wasadmin = false;
-    }
-}
-if (wasadmin){
-  ctx.reply('Benvenuto Amministratore ');
-  ctx.session.wasadmin = true;
-}else{
-  ctx.reply('non sei un admin, devi morire!');
-  ctx.session.wasadmin = false;
-}
+    .then(function(data) {
+      var wasadmin = false;
+      var n = Object.keys(data).length;
+      for (var i = 0; i < n && i < 10; i++) {
+        var admin = parseInt(data[i].user.id);
+        var user = parseInt(ctx.from.id);
+        if (admin == user) {
+          /*ctx.reply('Benvenuto Amministratore ');*/
+          wasadmin = true;
+          break;
+        } else {
+          /*ctx.reply('non sei un admin, devi morire!');*/
+          wasadmin = false;
+        }
+      }
+      if (wasadmin) {
+        ctx.reply('Benvenuto Amministratore ');
+        ctx.session.wasadmin = true;
+      } else {
+        ctx.reply('non sei un admin, devi morire!');
+        ctx.session.wasadmin = false;
+      }
 
-});
+    });
 });
 
-bot.command('Setbuildnote', (ctx) => {  //* Precedentemente /addA *//
-  if(ctx.session.wasadmin){
+bot.command('setbuildnote', (ctx) => { //* Precedentemente /addA *//
+  if (ctx.session.wasadmin) {
 
     ctx.reply('fatto!');
-    A = cut("/Setbuildnote ",getUserMsg(ctx));
-    console.log(cut("/Setbuildnote ",getUserMsg(ctx)));
-  }else{
+    A = cut("/setbuildnote ", getUserMsg(ctx));
+    console.log(cut("/setbuildnote ", getUserMsg(ctx)));
+  } else {
     ctx.reply('non hai i permessi!');
   }
 })
