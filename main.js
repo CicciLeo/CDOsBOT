e1f34ecc196a4ceab473456068c9e9e00f772705
francesco <fra>
2019-12-13 05:04:36 +0100

    Some Fixes, and adds
    ADDED: Login check in everycomand , File Json loading, json save, get for notes and notes.

--- main.js
+++ main.js
@@ -2,51 +2,57 @@
 const Telegraf = require('telegraf');
 const session = require('telegraf/session');
 const fs = require('fs');
-const csv = require('csv-parser');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const csvWriter = createCsvWriter({
-  path: 'notes.csv',
-  header: [{
-      id: 'note-id',
-      title: 'Note-id'
-    },
-    {
-      id: 'note-description',
-      title: 'Note-description'
-    },
-  ]
-});
 const sp = require('synchronized-promise');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 var A;
-var notes_list = ["/pippo", "/pluto", "/topolino"]; //Array contenente il nome di ciascun nota salvata
-var notes_description = ["test1", "test2", "test3"];
+var isAdmin = false;
+var notes_list = ["pippo", "pluto", "topolino"]; //Array contenente il nome di ciascun nota salvata
+var notes_description = ["cane", "gatto", "topo"];
 
-
-
-
-/*function saveNotes() { //BOTH VARS MUST BE ARRAY
-  var datanote = [""];
-  var temp = notes_list.lenght;
-  for (var notei = 0; temp > notei; notei++) {
-    datanote.push("{ note-id:"+"'"+notes_list[notei]+"'"+", node-description: "+"'"+ notes_description[notei]+"'} ");
-  }
-  csvWriter
-    .writeRecords(datanote)
-    .then(() => console.log('The CSV file was written successfully'));
-
-} */
-
-
-
-
-// buildnote_items
+//LOADING PROCESS
 bot.telegram.getMe().then((botInfo) => {
   bot.options.username = botInfo.username
   console.log("username: " + botInfo.username);
 });
 bot.use(session());
 
+
+fs.exists("/notes.json", function(exists) {
+  loadNotes();
+  console.log("\n\n\n" + notes_list);
+});
+//END LOADING
+
+function saveNotes() { //salva i due array delle note
+  var notes = function(note_l, note_d) {
+    this.notelist = note_l;
+    this.notedesc = note_d;
+  }
+  var notes_obj = new notes(notes_list, notes_description);
+  let data = JSON.stringify(notes_obj, null, 2);
+  fs.writeFile('notes.json', data, (err) => {
+    if (err) throw err;
+    console.log('Data written to file');
+  });
+}
+
+function loadNotes() { //carica gli array del file JSON nei due array delle note
+  fs.readFile('notes.json', (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    notes_list = notes.notelist;
+    notes_description = notes.notedesc;
+
+    console.log(notes_list);
+    console.log(notes_description);
+    bot.command("get", (ctx) => {
+      var command = cut("/get ", getUserMsg(ctx));
+      ctx.reply(notes_description[getNoteIDByName(command)]);
+    });
+
+  });
+
+}
 
 function cut(toRemove, string) {
   var ret = string.replace(toRemove, '');
@@ -60,106 +66,64 @@ function getUserMsg(context) {
 }
 
 function from(context) {
-
   return context.from.id;
-
 }
 
 function getChatID(context) {
   return context.chat.id;
 }
 
-
-bot.hears('Sei tu un bug ', (ctx) => {
-
-  ctx.reply('devi morire!');
-
-});
-
-
-bot.hears('buildnote', (ctx) => {
-  ctx.reply(A);
-});
-
-bot.hears('brb', (ctx) => {
-  ctx.reply("torno subito");
-});
-
-bot.hears('save', (ctx) => {
-  saveNotes();
-});
-
-bot.hears('@nunocodex', (ctx) => {
-
-
-  ctx.reply('prima o poi risponderà \no forse no');
-
-});
-
-
-bot.start((ctx) => {
-  ctx.session.wasadmin = false;
-  bot.telegram.getChatAdministrators(getChatID(ctx))
-    .then(function(data) {
-      var wasadmin = false;
-      var n = Object.keys(data).length;
-      for (var i = 0; i < n && i < 10; i++) {
-        var admin = parseInt(data[i].user.id);
-        var user = parseInt(ctx.from.id);
-        if (admin == user) {
-          /*ctx.reply('Benvenuto Amministratore ');*/
-          wasadmin = true;
-          break;
-        } else {
-          /*ctx.reply('non sei un admin, devi morire!');*/
-          wasadmin = false;
-        }
-      }
-      if (wasadmin) {
-        ctx.reply('Benvenuto Amministratore ');
-        ctx.session.wasadmin = true;
-      } else {
-        ctx.reply('non sei un admin, devi morire!');
-        ctx.session.wasadmin = false;
-      }
-
-    });
-});
-/*
-bot.command('set', (ctx) => { // Precedentemente /addA
-  if (ctx.session.wasadmin) {
-
-    ctx.reply('fatto!');
-    A = cut("/set ", getUserMsg(ctx));
-    console.log(cut("/set ", getUserMsg(ctx)));
+bot.command('savenotes', (ctx) => {
+  if (isAdmin) {
+    saveNotes();
+    ctx.reply("Notes has been saved");
   } else {
-    ctx.reply('non hai i permessi!');
+    ctx.reply("You dont have the permission to do that");
   }
-})
 
-bot.command('quit', (ctx) => {
-  ctx.session.wasadmin = false;
-})
-*/
-bot.launch();
+});
+
+bot.command('loadnotes', (ctx) => {
+  if (isAdmin) {
+    loadNotes();
+    ctx.reply("Notes has been loaded");
+  } else {
+    ctx.reply("You dont have the permission to do that");
+  }
+});
 
 
-bot.hears(notes_list, (ctx) => { //Funzione che si attiva ogni volta che un messaggio contiene il nome di una nota
 
-  ctx.reply("Bingo"); //Sostituire con il contenuto della nota (letto da file o da db IDK)
 
+bot.hears('@Shiberal', (ctx) => {
+  ctx.reply('prima o poi risponderà \no forse no');
+});
+
+
+function getNoteIDByName(name) {
+  for (var i = 0; i <= notes_list.length; i++) {
+    if (name == notes_list[i]) {
+      return i;
+    }
+  }
+}
+
+
+bot.command(notes_list, (ctx) => { //Funzione che si attiva ogni volta che un messaggio contiene il nome di una nota
+  var nome_nota = cut("/", getUserMsg(ctx));
+  ctx.reply(notes_description[getNoteIDByName(nome_nota)]);
 });
 
 bot.command('notes', (ctx) => {
   ctx.reply("List of notes of CleanDroidOS\n" + notes_list.join("\n"));
 });
 
-bot.command('addnote', (ctx) => {
-  if (ctx.session.wasadmin) {
-    var new_note = cut("/addnote ", getUserMsg(ctx)).split(" "); // qui prendi il
+bot.command('set', (ctx) => { // EX ADDNOTE NOW SET
+  if (isAdmin) {
+    var new_note = cut("/set ", getUserMsg(ctx)).split(" ");
     if (notes_list.indexOf(new_note[0]) != -1) {
       //Gestire se la nota è già presente
-      ctx.reply("Esiste già GG");
+      ctx.reply("Already exists a note with this name: " + new_note[0]);
     } else {
       notes_list.push("/" + new_note[0]);
 
@@ -168,8 +132,32 @@ bot.command('addnote', (ctx) => {
     ctx.reply('non hai i permessi!');
   }
 
-  bot.command('notes', (ctx) => {
-    ctx.reply("List of notes of CleanDroidOS\n" + notes_list.join("\n")); //Aggiorna la lista dei comandi xD
-  });
+  ctx.reply("List of notes of CleanDroidOS\n" + notes_list.join("\n"));
+});
 
-})
+
+
+bot.command("get", (ctx) => {
+  var command = cut("/get ", getUserMsg(ctx));
+  ctx.reply(notes_description[getNoteIDByName(command)]);
+});
+
+
+bot.command((ctx) => {
+  bot.telegram.getChatMember(getChatID(ctx), from(ctx))
+    .then(function(data) {
+      //ctx.reply("---DEBUG-DATA---\n"+data+"\n---DEBUG-DATA.status---\n"+ data.status);
+      if ((data.status != "creator") && (data.status != "administrator")) {
+        isAdmin = false;
+      } else {
+        isAdmin = true;
+      }
+      //ctx.reply(isAdmin);
+    });
+
+
+});
+
+
+
+bot.launch();
